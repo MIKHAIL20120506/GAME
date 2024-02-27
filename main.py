dcb6e54498bc443503250e4bc7b3493b6b6d09ec
MIKHAIL20120506 <159912401+MIKHAIL20120506@users.noreply.github.com>
2024-02-27 17:29:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ screen_heigh = 600
 screen_width = 600
 screen = pygame.display.set_mode((screen_width, screen_heigh))
 game_size = 10
+box_size = screen_width/game_size
+
+def draw_field():
+    y_count = 0
+    while y_count < game_size:
+        x_count = 0
+        while x_count < 10:
+            pygame.draw.rect(screen, "green", (x_count*box_size, y_count*box_size, box_size, box_size),1)
+            x_count += 1
+        y_count += 1
+
+    
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-    pygame.draw.rect(screen, "green", (100,100,50,50))
+    screen.fill("black")
+    draw_field()
     pygame.display.update()
