23bcc09bf5269552a127e9f646546cf2d4203008
MIKHAIL20120506 <159912401+MIKHAIL20120506@users.noreply.github.com>
2024-09-09 19:35:09 +0300

    Add files via upload

--- misha.py
+++ misha.py
@@ -1,25 +1,18 @@
-import random 
-print("Здраствуйте это игра spider man:Begin Сюжет: Bас укусил радиоактивный паук и у вас поевились силы ходить по стенам стрелять паутиной вы сильные и у вас чуйка")
-playerhp = 100
-playerenergy = 100
-bosshp = 100
-playeractiv = None
-bossact = None
-#x=input("выберете 1.пойти драться ")
-#if x== "1":
-#    print("вы полетели на паутине к боссу")
-x=input(f"вы прилетели к боссу у вас есть выбор 1."+
-        f"ударить рукой но усталость минус 5(у вас {playerenergy} "+
-        f"сил каждый удар рукой по -5 сил)2.ударить ногой -15 сил ")
-if x== "1":
-    playeractiv = "бить рукой"
-bossactiv = ["бить рукой","уворачиваться"]
-bossrandom = random.choice(bossactiv)
-if bossrandom == "бить рукой":
-    playerhp -= 5
-    print(f"босс ударил вас рукой у вас осталось {playerhp} хп")
-if bossrandom == "уворачиваться":
-    print("босс увернулся хп ему не наносяться ")
-#if x== "2":
-#    print("вы ударирили ногой минус 15 хп босса у него 85 хп")
+import random
+import time
+
+x = random.randint(1, 100)
+y = random.randint(1, 100)
+h = time.time()
+m = x + y
+
+n = input()
+z = time.time()
+print(z - h)
+
+
+
+
+
+
 
